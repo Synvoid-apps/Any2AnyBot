f4e1f19fa36c8c008ee5c1b13eef43f77da0feee
Synvoid-apps <adityaraj8578095389@gmail.com>
2025-12-26 13:17:39 +0530

    Update db.py

--- db.py
+++ db.py
@@ -5,15 +5,16 @@ from datetime import datetime
 
 load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
+
 client = MongoClient(MONGO_URI)
 db = client["any2any"]
 users = db["users"]
 
-def get_user(user_id):
-    user = users.find_one({"_id": user_id})
+def get_user(uid):
+    user = users.find_one({"_id": uid})
     if not user:
         user = {
-            "_id": user_id,
+            "_id": uid,
             "is_vip": False,
             "today_count": 0,
             "last_use": str(datetime.now().date())
@@ -21,27 +22,14 @@ def get_user(user_id):
         users.insert_one(user)
     return user
 
-def update_usage(user_id):
-    user = get_user(user_id)
-    last = user["last_use"]
+def usage_allowed(uid):
+    user = get_user(uid)
+    if user["is_vip"]: return True
+    return user["today_count"] < 10
+
+def update_usage(uid):
+    user = get_user(uid)
     today = str(datetime.now().date())
-
-    if last != today:
-        users.update_one({"_id": user_id},
-                         {"$set": {"today_count": 0, "last_use": today}})
-
-    users.update_one({"_id": user_id},
-                     {"$inc": {"today_count": 1}})
-
-def usage_allowed(user_id):
-    user = get_user(user_id)
-    if user["is_vip"]:
-        return True
-
-    if user["today_count"] >= 10:
-        return False
-    return True
-
-def reset_limits():
-    today = str(datetime.now().date())
-    users.update_many({}, {"$set": {"today_count": 0, "last_use": today}})
+    if user["last_use"] != today:
+        users.update_one({"_id": uid}, {"$set": {"today_count": 0, "last_use": today}})
+    users.update_one({"_id": uid}, {"$inc": {"today_count": 1}})
