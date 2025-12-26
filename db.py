c9e2ecac000f149cfc82bc467e3c8658166ca799
Synvoid-apps <adityaraj8578095389@gmail.com>
2025-12-26 14:22:06 +0530

    Update db.py

--- db.py
+++ db.py
@@ -9,7 +9,9 @@ MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
 db = client["any2any"]
 users = db["users"]
+files = db["files"]  # Cloud storage
 
+# ---- USER SYSTEM ----
 def get_user(uid):
     user = users.find_one({"_id": uid})
     if not user:
@@ -33,3 +35,14 @@ def update_usage(uid):
     if user["last_use"] != today:
         users.update_one({"_id": uid}, {"$set": {"today_count": 0, "last_use": today}})
     users.update_one({"_id": uid}, {"$inc": {"today_count": 1}})
+
+# ---- CLOUD FILE STORAGE ----
+def save_file(uid, file_id, file_name):
+    files.insert_one({
+        "uid": uid,
+        "file_id": file_id,
+        "name": file_name
+    })
+
+def list_files(uid):
+    return list(files.find({"uid": uid}).sort("_id", -1).limit(10))
